9b642f39eb67c4a02c83e28ca6337b70b09711fa
f-guedes <fernando.hpg2@gmail.com>
2022-10-06 18:18:45 -0400

    collections

--- src/week4/Collections.java
+++ src/week4/Collections.java
@@ -40,28 +40,74 @@ public class Collections {
 		 */
 		Set<String> states = new HashSet<String>();
 		states.add("Alabama");
-		states.add("Alabama");
-		states.add("Alaska");
 		states.add("Alaska");
 		states.add("Arizona");
 		states.add("Arkansas");
 		states.add("California");
+		states.add("Colorado");
+		states.add("Connecticut");
+		states.add("Delaware");
+		states.add("Florida");
+		states.add("Georgia");
+		states.add("Hawaii");
+		states.add("Idaho");
+		states.add("Illinois");
+		states.add("Indiana");
+		states.add("Iowa");
+		states.add("Kansas");
+		states.add("Kentucky");
+		states.add("Louisiana");
+		states.add("Maine");
+		states.add("Maryland");
 		states.add("Massachusetts");
 		states.add("Michigan");
+		states.add("Minnesota");
+		states.add("Mississippi");
+		states.add("Missouri");
+		states.add("Montana");
+		states.add("Nebraska");
+		states.add("Nevada");
+		states.add("New Hampshire");
+		states.add("New Jersey");
+		states.add("New Mexico");
+		states.add("New York");
+		states.add("North Carolina");
+		states.add("North Dakota");
+		states.add("Ohio");
+		states.add("Oklahoma");
+		states.add("Oregon");
+		states.add("Pennsylvania");
+		states.add("Rhode Island");
+		states.add("South Carolina");
+		states.add("South Dakota");
+		states.add("Tennessee");
+		states.add("Texas");
+		states.add("Utah");
+		states.add("Vermont");
+		states.add("Virginia");
+		states.add("Washington");
+		states.add("West Virginia");
+		states.add("Wisconsin");
+		states.add("Wyoming");		
+		states.add(null);
+		states.add("Alabama");
+		states.add("Alaska");
 		states.add("Massachusetts");
 		states.add("Michigan");
 		states.add("Ohio");
-		states.add(null);
 		
 		System.out.println(states.size());
 		System.out.println(states.contains("Ohio"));
-		if (states.contains("Alabama")) {
-			states.remove("Alabama");
+		
+		if (states.contains(null)) {
+			states.remove(null);
+			System.out.println(states.size());
 		}
 		
-		for (String state : states) {
-			System.out.println(state);
-		}
+		
+//		for (String state : states) {
+//			System.out.println(state);
+//		}
 		
 		
 		/* Map
